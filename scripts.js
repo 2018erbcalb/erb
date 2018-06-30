cabb4dd64eba5e784cd31080aa6ae8bae57247f4
CalebErb <iamallerb5@gmail.com>
2018-06-30 19:02:31 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+function revealSpoiler() {
+    var x = document.getElementById("spoiler");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.left = "-15";
+    } else {
+        document.getElementById("topBtn").style.left = "-65";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+  
+  
+  
